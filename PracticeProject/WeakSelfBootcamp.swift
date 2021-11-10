6d84b5e82e23741755a15883a51a6c27769e59fc
Luka <vujnovacluka89@gmail.com>
2021-11-10 13:00:06 +0100

    weak self bootcamp completed

---
+++ PracticeProject/WeakSelfBootcamp.swift
@@ -0,0 +1,82 @@
+//
+//  WeakSelfBootcamp.swift
+//  PracticeProject
+//
+//  Created by Luka Vujnovac on 10.11.2021..
+//
+
+import SwiftUI
+
+struct WeakSelfBootcamp: View {
+    
+    @AppStorage("count") var count: Int?
+    
+    init() {
+        count = 0
+    }
+    
+    var body: some View {
+        NavigationView{
+            NavigationLink("Navigate") { 
+                WeakSelfSecondScreen()
+            }
+            .navigationTitle("Screen 1")
+        }
+        .overlay(
+            Text("\(count ?? 0)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.green)
+                .cornerRadius(10)
+            , alignment: .topTrailing
+            )
+    }
+}
+
+struct WeakSelfSecondScreen: View {
+    
+    @StateObject var vm = WeakSelfSecondScreenViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("second view")
+                .font(.largeTitle)
+                .foregroundColor(.red)
+            
+            if let data = vm.data {
+                Text(data)
+            }
+        }
+    }
+}
+
+class WeakSelfSecondScreenViewModel: ObservableObject {
+    
+    @Published var data: String? = nil
+    
+    init() {
+        print("initalize now")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount + 1, forKey: "count")
+        getData()
+    }
+    
+    deinit {
+        print("deinitialize now")
+        
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount - 1, forKey: "count")
+    }
+    
+    func getData() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 500) { [weak self] in 
+            self?.data = "NEW DATA!!!"
+        }
+    }
+}
+
+struct WeakSelfBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        WeakSelfBootcamp()
+    }
+}
