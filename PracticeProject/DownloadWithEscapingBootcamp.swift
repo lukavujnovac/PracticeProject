0ef479e0e00bd15abcb5e585b4c3ce9fc5d270a8
Luka <vujnovacluka89@gmail.com>
2021-11-14 21:08:28 +0100

    Downloading data with escaping bootcamp completed

---
+++ PracticeProject/DownloadWithEscapingBootcamp.swift
@@ -0,0 +1,84 @@
+//
+//  DownloadWithEscapingBootcamp.swift
+//  PracticeProject
+//
+//  Created by Luka Vujnovac on 14.11.2021..
+//
+
+import SwiftUI
+
+struct PostModel: Codable, Identifiable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class DownloadWithEscapingViewModel: ObservableObject {
+    
+    @Published var posts: [PostModel] = []
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return}
+        
+        downloadData(from: url) { returnedData in
+            if let data = returnedData {
+                guard let newPosts = try? JSONDecoder().decode([PostModel].self, from: data) else {return}
+                DispatchQueue.main.async { [weak self] in
+                    self?.posts = newPosts
+                }
+            }else {
+                print("no data returned")
+            }
+        }
+    }
+    
+    func downloadData(from url: URL, completitonHandler: @escaping (_ data: Data?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard 
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300
+            else {
+                print("error downloading data")
+                completitonHandler(nil)
+                return
+            }
+            
+            completitonHandler(data)
+            
+        }.resume()
+    } 
+}
+
+struct DownloadWithEscapingBootcamp: View {
+    
+    @StateObject var vm = DownloadWithEscapingViewModel()
+    
+    var body: some View {
+        List{
+            ForEach(vm.posts) { post in 
+                VStack(alignment: .leading){
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundColor(.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+}
+
+struct DownloadWithEscapingBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadWithEscapingBootcamp()
+    }
+}
